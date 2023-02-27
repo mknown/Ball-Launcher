aa36ae3dcfe5119adba05b50a610bdaed265d79a
MachineSpeaker <unknowncoderbase@gmail.com>
2023-02-27 13:31:11 +0530

    Added Launching Option to the ball

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -6,6 +6,9 @@ public class BallHandler : MonoBehaviour
 {
     private Camera mainCamera;
     [SerializeField] private Rigidbody2D currentBallRigidBody ;
+    [SerializeField] private SpringJoint2D currentBallSpringJoint ;
+    [SerializeField] float counter=0f;
+    bool isDragged;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,17 +18,38 @@ public class BallHandler : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        if (!Touchscreen.current.primaryTouch.press.isPressed)
+    {   
+        if (currentBallRigidBody==null) { return; }
+
+        else if (!Touchscreen.current.primaryTouch.press.isPressed)
         {
-            currentBallRigidBody.bodyType=RigidbodyType2D.Dynamic;
+            if (isDragged)
+            {
+                LaunchBall();
+            }
+           
             return;
             
         }
+        isDragged = true;
         currentBallRigidBody.isKinematic=true;
         Vector2 touchData=Touchscreen.current.primaryTouch.position.ReadValue();
         Vector3 worldPoint = mainCamera.ScreenToWorldPoint(touchData);
         currentBallRigidBody.position = worldPoint;
+
+    }
+    private void LaunchBall()
+    {
+        currentBallRigidBody.bodyType = RigidbodyType2D.Dynamic;
+        currentBallRigidBody=null;
+
+        Invoke("Detach", counter);
+
+    }
+    void Detach()
+    {
+        currentBallSpringJoint.enabled=false;
+        currentBallSpringJoint=null;
 
     }
 }
